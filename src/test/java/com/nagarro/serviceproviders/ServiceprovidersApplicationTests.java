b0e0cadf0254d2c19a965792af4eb7b70676c4f0
Ojas Modi <ojas.modi@nagarro.com>
2021-07-17 23:03:51 +0530

    initial commit

---
+++ src/test/java/com/nagarro/serviceproviders/ServiceprovidersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nagarro.serviceproviders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceprovidersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
